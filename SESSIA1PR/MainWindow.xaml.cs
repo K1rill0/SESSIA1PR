b14be439e85c17121e84acd42192a06221f110e7
K1rill0 <kirya.moiseev.03@mail.ru>
2022-05-18 12:40:00 +0300

    32

--- SESSIA1PR/MainWindow.xaml.cs
+++ SESSIA1PR/MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -25,6 +26,31 @@ namespace SESSIA1PR
             InitializeComponent();
         }
 
-       
+        private void Image_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            MessageBox.string;
+
+
+            Random r = new Random();
+            int a = 5;//количество знаков до запятой
+            double d = r.Next(10 ^ a) + r.NextDouble();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            if ((Login.Text == "") || (Password.Text == "") || (Kod.Text == "")) 
+            {
+                MessageBox.Show("Ошибка");
+            }
+            else
+            {
+                MessageBox.Show("Поздравляем, вы вошли!");
+            }
+        }
     }
 }
